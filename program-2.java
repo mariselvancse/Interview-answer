ac550cab0b360c8ef22a6691320316841ecde114
mariselvancse <mariselvancs@gmail.com>
2025-11-28 18:18:02 +0530

    Update program-2.java

--- program-2.java
+++ program-2.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-
 public class program2 {
     public static void main(String[] args) {
         System.out.println("Enter number:");
         Scanner sin =new Scanner(System.in);
-
         int N=sin.nextInt();
         int value=1;
         for(int i=1;i<=N;i++)
