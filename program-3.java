bc1629598c2da1fddbc0d467f3e7e8d600b23609
mariselvancse <mariselvancs@gmail.com>
2025-11-28 18:16:02 +0530

    Update program-3.java
    program -3 was completed

--- program-3.java
+++ program-3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class program-3 {
+public class program3 {
     public static void main(String[] args) {
         System.out.println("Enter number:");
         Scanner sin =new Scanner(System.in);
